240dfebf964bf62a580446d7e248dce3ed1e3f4a
Kiran Kshirsagar <87958527+kirankshirsagar478@users.noreply.github.com>
2025-11-22 20:41:02 +0530

    pattern 3 added

---
+++ pattern/pattern3.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print3(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <=i; j++) {
+            cout <<j << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n;
+        cin >> n;
+        print3(n);
+    }
+}
+
+// input:
+//     2
+//     3
+//     5
+// output:
+//     1 
+//     1 2 
+//     1 2 3 
+//     1 
+//     1 2 
+//     1 2 3 
+//     1 2 3 4 
+//     1 2 3 4 5 
