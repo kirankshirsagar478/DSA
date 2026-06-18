1cefe3ae0a338662093a9aefd80763cc1328e937
kirankshirsagar478 <kirankshirsagar478@gmail.com>
2026-06-18 12:50:40 +0530

    pattern 13 done

--- pattern/pattern12.cpp
+++ pattern/pattern12.cpp
@@ -1,8 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print8(int n) {
-    for (int i = 0; i < n; i++) {
+void print12(int n) {
+    int space = 2* (n - 1);
+    for (int i = 1; i <= n; i++) {
+        //numbers
+        for (int j = 1; j<= i; j++) {
+            cout << j;
+        }
+
+        //spaces
+        for (int j = 1; j <= space; j++) {
+            cout << " ";
+        }
+        
+
+        //numbers
+        for (int j = i ; j >= 1; j--) {
+            cout << j;
+        }
+        cout << endl;
+        space -= 2;
     }
 }
 
@@ -12,7 +30,7 @@ int main() {
     for (int i = 0; i < t; i++) {
         int n;
         cin >> n;
-        print8(n);
+        print12(n);
     }
 }
 
@@ -20,12 +38,8 @@ int main() {
 //     1
 //     5
 // output:
-// *
-// **
-// ***
-// ****
-// *****
-// ****
-// ***
-// **
-// *
+// 1        1       [1,8,1]
+// 12      21       [2,6,2]
+// 123    321       [3,4,3]
+// 1234  4321       [4,2,4]
+// 1234554321       [5,0,5]
